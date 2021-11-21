a2bd1c56fc811eccaf93e7add617d42fb79bac1b
kritarth <kritarth2121@gmail.com>
2021-11-21 19:51:49 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { useState } from "react";
-import "./App.css";
-import DisplayData from "./DisplayData";
-import UploadFile from "./UploadFile";
-import { DragTable } from "./DragTable";
+import { useState } from 'react'
+import './App.css'
+import UploadFile from './UploadFile'
+import { DragTable } from './DragTable'
 function App() {
-  const [uploadedExcelData, setUploadedExcelData] = useState([]);
-  const uploadedExcelDataHandler = (data) => {
-    setUploadedExcelData(data);
-  };
-  return (
-    <>
-    <div className="container">
-      <UploadFile onUploadExcelFile={uploadedExcelDataHandler} />
-
-
-      
-      {/* <DisplayData excelData={uploadedExcelData} /> */}
-    </div>
-    <DragTable excelData={uploadedExcelData}/>
-</>
-  );
+    const [uploadedExcelData, setUploadedExcelData] = useState([])
+    const uploadedExcelDataHandler = (data) => {
+        setUploadedExcelData(data)
+    }
+    return (
+        <>
+            <div className="container">
+                <UploadFile onUploadExcelFile={uploadedExcelDataHandler} />
+            </div>
+            <DragTable excelData={uploadedExcelData} />
+        </>
+    )
 }
 
-export default App;
+export default App
